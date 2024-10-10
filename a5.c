f482c4a0b02a1b6d927537179c55d66834c9ea3c
Hyeonwoo Heo <hyeonwoo.heo@gmail.com>
2024-10-10 01:17:41 -0400

    reduce time by updating get_count

--- a5.c
+++ a5.c
@@ -183,12 +183,10 @@ int get_count (struct point_tree *head, struct circle circle) {
     }
 
     int count = 0;
-    
-    int x = head->point.x - circle.x;
 
-    if(x < -circle.radius) {
+    if (head->point.x < circle.x - circle.radius) {
         count += get_count(head->right, circle);
-    } else if(x > circle.radius) {
+    } else if (head->point.x > circle.x + circle.radius) {
         count += get_count(head->left, circle);
     } else {
         if (is_in_radius(head->point, circle)) {
